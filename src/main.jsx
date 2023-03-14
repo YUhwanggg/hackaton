200b02c0b14dbe8c0e2f7551b88d013e124ff74f
keraemoe <weekera@gmail.com>
2023-03-14 07:38:56 +0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
-import App from "./App/App";
+import App from '../src/app/App'
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
